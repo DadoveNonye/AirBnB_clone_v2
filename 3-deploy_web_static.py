2b7002a828815f5535a52dd33b7ad7a52d0d1290
dadovenonye <chinonyenmecha@yahoo.com>
2024-02-03 16:19:43 -0800

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,19 +10,42 @@ from os import path
 from 2-do_deploy_web_static import do_deploy, do_pack
 env.hosts = ['18.206.197.202', '54.237.125.178']
 
+def do_pack():
 
-def deploy():
     """
-    Create and distribute an archive to web servers
+    Generates a .tgz archive from the contents of the web_static folder
     """
-    archive_path = do_pack()
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    return 'versions/{}'.format(archive) if create.succeeded else None
 
-    if archive_path is None:
+def do_deploy(archive_path):
+    """
+    Deploy the archive to the web servers.
+
+    Args:
+        archive_path (str): The path to the archive file to be deployed.
+
+    Returns:
+        bool: True if deployment is successful, False otherwise.
+    """
+    if not exists(archive_path):
         return False
 
-    deploy_result = do_deploy(archive_path)
-
-    return deploy_result
-
-if __name__ == "__main__":
-    deploy()
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
