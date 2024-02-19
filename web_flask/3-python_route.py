a78ed18628b54f5bdfb8226289dbce2af2e873ef
dadovenonye <chinonyenmecha@yahoo.com>
2024-02-18 20:55:58 -0800

    working with flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns hbnb"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return 'HBNB!'
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """returns C with the passed variable"""
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """returns python with the passed variable"""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
