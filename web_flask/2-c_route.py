75a54667704a4257879923535e20f3312dde672f
dadovenonye <chinonyenmecha@yahoo.com>
2024-02-18 20:31:03 -0800

    working with flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-# A script that starts a Flask web application
+"""
+A script that starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """returns hbnb"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns hbnb"""
     return 'HBNB!'
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_text(text):
+    """returns C with the passed variable"""
     text_with_spaces = escape(text.replace('_', ' '))
     return f'C {text_with_spaces}'
 
