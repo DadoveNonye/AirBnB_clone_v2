2a02d601d5f59560f2adfd60da475edab0660288
dadovenonye <chinonyenmecha@yahoo.com>
2024-02-19 17:19:45 -0800

    working with flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,7 +40,7 @@ def numbertemplates(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """Returns HTML page with even or odd message"""
-    return render_template('number_odd_even.html', n=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
